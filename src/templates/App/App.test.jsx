1382b3d975e15ffe3bfe34013c00326f6c4c2055
Julys Martins <julystube@gmail.com>
2021-04-13 20:51:15 -0300

    adicionando testes

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -4,16 +4,8 @@ import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
 import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-
-  const headingContainer = screen.getByRole('heading', { name: 'hello' })
-    .parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-
-  expect(headingContainer).toMatchSnapshot();
-
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
