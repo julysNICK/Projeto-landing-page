744b3080aa953d901f2de176af71a2b9d031e2e3
Julys Martins <julystube@gmail.com>
2021-04-25 23:54:27 -0300

    adicioanndo populçao de dados

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,35 +1,76 @@
 import { useEffect, useState } from 'react';
-import { Heading } from '../../components/Heading';
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
+
 import * as Styled from './styles';
+
 import { mapData } from '../../api/map-data';
+
+import { Heading } from '../../components/Heading';
+import { GridTwoColum } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
+import { mockBase } from '../Base/mock';
+import { Base } from '../Base';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
-
   useEffect(() => {
     const load = async () => {
-      const data = await fetch(
-        'http://localhost:1337/pages/?slug=landing-page',
-      );
-      const json = await data.json();
-      const pageData = mapData(json);
-      setData(pageData[0]);
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-page',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+        setData(pageData[0]);
+      } catch (e) {
+        console.log(e);
+        setData(undefined);
+      }
     };
-
     load();
   }, []);
-
   if (data === undefined) {
-    return <h1>Página não encontrada</h1>;
+    return <PageNotFound />;
   }
-
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColum key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
